0c60d3b0c84bf8ffb2eb17d7b72d414f2160aefe
Julien Le Mee <julemee@gmail.com>
2022-10-14 15:14:07 -0400

    added logo and style navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Navbar() {
   return (
     <nav className="navbar">
-      <h3>This my navbar</h3>
+      <img src="../images/airbnb-logo.svg" className='nav--logo' alt="airbnb-logo" />
     </nav>
   )
 }
